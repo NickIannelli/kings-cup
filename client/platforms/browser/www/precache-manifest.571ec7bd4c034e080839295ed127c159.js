8d7f15940a4c3d1056bd5b1ecb2edae55e9761cb
Nick Iannelli <nicholas.j.iannelli@gmail.com>
2020-01-02 15:32:28 +1100

    Initialize cordova application for cross-platform deployment

---
+++ client/platforms/browser/www/precache-manifest.571ec7bd4c034e080839295ed127c159.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c7f5d139914b00e81f7cf28366d846",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f601c3283f53fcf224de",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "6333909158b1718fd708",
+    "url": "/static/css/main.572de59a.chunk.css"
+  },
+  {
+    "revision": "f601c3283f53fcf224de",
+    "url": "/static/js/2.eadcc807.chunk.js"
+  },
+  {
+    "revision": "fabe6e8bc1203e177b62cd9daebbf511",
+    "url": "/static/js/2.eadcc807.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6333909158b1718fd708",
+    "url": "/static/js/main.a199ef19.chunk.js"
+  },
+  {
+    "revision": "e3bccf42565464fa8bb1",
+    "url": "/static/js/runtime-main.823fa4f3.js"
+  }
+]);
